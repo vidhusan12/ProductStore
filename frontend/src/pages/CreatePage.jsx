83ad128900dacd192669dce6691fa664d790e109
vidhusan12 <vidhusan709@gmail.com>
2025-07-05 20:33:26 +1200

    added the input for the createPage

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,5 +1,6 @@
-import { Box, Container, Heading, VStack } from '@chakra-ui/react'
+import { Box, Button, Container, Heading, Input, VStack } from '@chakra-ui/react'
 import React, { useState } from 'react'
+import { useColorModeValue } from '../components/ui/color-mode'
 
 const CreatePage = () => {
   const [newProduct, setNewProduct] = useState({
@@ -16,7 +17,35 @@ const CreatePage = () => {
           Create New Page
         </Heading>
 
-        <Box>
+        <Box
+          w={"full"}
+          bg={useColorModeValue("white", "gray.800")}
+          p={6}
+          rounded={"lg"}
+          shadow={"md"}
+        >
+
+          <VStack spacing={4}>
+            <Input
+              placeholder='Product Name'
+              name='name'
+              value={newProduct.name}
+              onClick={e => setNewProduct({ ...newProduct, name: e.target.value })}
+            />
+            <Input
+              placeholder='Price'
+              name='price'
+              value={newProduct.price}
+              onClick={e => setNewProduct({ ...newProduct, price: e.target.value })}
+            />
+            <Input
+              placeholder='Image URL'
+              name='image'
+              value={newProduct.image}
+              onClick={e => setNewProduct({ ...newProduct, image: e.target.value })}
+            />
+            <Button colorScheme={"blue"} >Add Product</Button>
+          </VStack>
 
         </Box>
       </VStack>
